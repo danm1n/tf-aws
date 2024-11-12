8f4a7ae883685c701a95573943af7d3aa9d289b2
Daniel Minter <danielminter13@gmail.com>
2024-11-12 12:36:03 +0200

    Update s3.tf

--- modules/s3/s3.tf
+++ modules/s3/s3.tf
@@ -7,7 +7,7 @@ resource "aws_s3_bucket" "codepipeline_artifacts" {
     Environment = "Test"
   }
 }
-#
+
 # resource "aws_s3_bucket_acl" "codepipeline_artifacts_acl" {
 #   bucket = aws_s3_bucket.codepipeline_artifacts.id
 #   acl    = "private"
